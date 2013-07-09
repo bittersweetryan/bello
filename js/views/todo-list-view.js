e0dfc52f80f2a00aae5494f708bcc12828283beb
Ryan Anklam <ryan.anklam@gmail.com>
2013-07-09 16:56:44 -0500

    factored out addToList function

--- js/views/todo-list-view.js
+++ js/views/todo-list-view.js
@@ -18,6 +18,7 @@ app.views.TodoListView = Backbone.View.extend({
 		this.listenTo( app.collections.todos, 'remove', this.removeView );
 	},
 
+	//when the list renders tell it to add all the todos from the collection
 	render : function( ){
 
 		app.collections.todos.forEach(
@@ -26,30 +27,36 @@ app.views.TodoListView = Backbone.View.extend({
 	},
 
 	arrangeCards : function( ){
+
 		_.each( this.taskViews, function( view ){
 
-			//compare the list of the view to its list
+			//compare the sections list to the list value of the model, if they don't match
+			//put it in the right list
 			if( view.$el.parents( 'section' ).attr( 'id' ) !== view.model.get( 'list' ) ){
-				view.$el.appendTo( this.$el.find( '#' + view.model.get( 'list' ) ).find( 'ol' ) );
+				this.addToList( view.model.get( 'list' ), view.$el );
 			}
 		}, this);
 	},
 
 	add : function(){
+		//proxy to append call append task with no arguments
 		this.appendTask();
 	},
 
 	appendTask : function( task ){
-
+		//create a new task view
 		var taskView = new app.views.TaskView( task ).render();
-
+		//add it to the internal array
 		this.taskViews.push( taskView );
-
-		this.$el.find( '#' + ( taskView.model.get( 'list' ) || 'todo' ) ).find( 'ol' ).append( taskView.$el );
+		//append it to the right list
+		this.addToList( taskView.model.get( 'list' ), taskView.$el );
 
 		taskView.setFocus();
 	},
 
+	addToList : function( list, $el ){
+		this.$el.find( '#' + ( list || 'todo' ) ).find( 'ol' ).append( $el );
+	},
 	//when listening to a remove event on a collection the event listener gets 2 arguments: 
 	//model that was removed and the new collection
 	removeView: function( removed ){
